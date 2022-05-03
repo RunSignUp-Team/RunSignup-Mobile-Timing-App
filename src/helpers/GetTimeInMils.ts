c493c59236ce39082551d4aa000a97c32aa2ed71
nathanael-rsu <nathanael@runsignup.com>
2022-05-03 11:06:45 -0400

    #8 More fixes for times / bibs

--- src/helpers/GetTimeInMils.ts
+++ src/helpers/GetTimeInMils.ts
@@ -1,21 +1,35 @@
-// Supported Time Formats
+// Test Formats (Supported)
 // hh:mm:ss:ms | hh:mm:ss.ms (equivalent)
 // hh:mm:ss
 //    mm:ss
 //    mm:ss.ms
 //       ss.ms
+// And all of the above, but with only one digit not two
+
+// Test Formats (Unsupported)
+// dd
+// dd: | dd.
+// :dd | .dd
 
 // Returns float value of time string
 export default function getTimeInMils(clockTime: string): number {
-	if (/^\d{1,2}(:\d{1,2})?(:\d{1,2})?(\.\d{1,2})?$/gm.test((clockTime))) {
+	if (/^(\d{1,2}:){0,2}\d{1,2}((\.|:)\d{1,2})$/gm.test((clockTime))) {
 		// Clock Time split into array
 		const clockSplit: Array<string> = clockTime.split(/[:.]+/);
 
-		// If format is 00:00:00:00, effectively convert it to 00:00:00.00,
+		// If format is hh:mm:ss:ms, effectively convert it to hh:mm:ss.ms,
 		// so at the switch statement, there are a maximum of three elements in the array,
 		// with ss.ms being treated as a decimal number
 		if (clockSplit.length === 4) {
 			clockSplit[2] = clockSplit[2] + "." + clockSplit[3];
+			clockSplit.pop();
+		}
+
+		// If format is ss.ms, it is initially stored as [ss, ms],
+		// But we want to convert it to [ss.ms]
+		if (!clockTime.includes(":") && clockTime.includes(".")) {
+			// console.log(clockTime);
+			clockSplit[0] = clockSplit[0] + "." + clockSplit[1];
 			clockSplit.pop();
 		}
 
@@ -39,6 +53,7 @@ export default function getTimeInMils(clockTime: string): number {
 				timeInMils = timeSplit[0] * 1000;
 				break;
 		}
+
 		return timeInMils;
 	} else {
 		return -1;
