a6008fdc4205837b7ddfdc3ae6d78e8fbe338c43
nathanael-rsu <nathanael@runsignup.com>
2022-10-13 12:48:21 -0400

    #64 oAuth / README.md updates

--- src/constants/oAuth2Constants.ts
+++ src/constants/oAuth2Constants.ts
@@ -1,15 +1,3 @@
-export const CLIENT_ID = 2;
+export const CLIENT_ID = 2; // For DEV: 4
 export const REDIRECT_URI = "com.rsu.mobile-timing-app://"; // Should be identical to scheme in app.json
-export const RUNSIGNUP_URL = "https://runsignup.com/";
-
-// See README.md for more information on changing hosts file enable mobiletest.example.com
-
-// If you are testing in DEV and want to run on the production server,
-// CLIENT_ID = 3
-// REDIRECT_URI = exp://mobiletest.example.com:19000
-// RUNSIGNUP_URL = https://runsignup.com/
-
-// If you are testing in DEV and want to run on a test server,
-// CLIENT_ID = 4
-// REDIRECT_URI = exp://localhost:19000
-// RUNSIGNUP_URL = https://test5.runsignup.com/
+export const RUNSIGNUP_URL =  "https://runsignup.com/"; // For DEV: https://test5.runsignup.com/
